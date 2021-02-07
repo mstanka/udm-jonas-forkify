90c5522c00cdfd14571e80574be8e22fe1329e47
mstanka <mice78.mm@gmail.com>
2021-02-07 14:57:34 +0100

    render pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable'; // packages for polyfiling
 import 'regenerator-runtime/runtime';
@@ -41,7 +42,10 @@ const controlSearchResults = async () => {
 
     // 3) render results
     //resultsView.render(model.state.search.results);
-    resultsView.render(model.getSearchResultsPage());
+    resultsView.render(model.getSearchResultsPage(6));
+
+    // 4) render initial pagination buttons
+    paginationView.render(model.state.search)
   } catch (err) {
     console.log(err);
   }
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -20,8 +20,7 @@ class ResultsView extends View {
           </figure>
           <div class="preview__data">
             <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-           
+            <p class="preview__publisher">${result.publisher}</p>           
           </div>
         </a>
       </li>
