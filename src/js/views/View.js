fc18065fde1b478958574de21ed518b06cc2fbe2
mstanka <mice78.mm@gmail.com>
2021-02-16 20:16:02 +0100

    how to write docs for functions

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,15 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the recieved object to the DOM
+   * @param {Object | Object[] } data The data to be rendered (e.g. recipe)
+   * @param {bollean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is rendered if render=false
+   * @this {Object} View instance
+   * @author author
+   * @todo Finish implementation
+   */
   render(data, render = true) {
     // if no data or if not array or if empty array
     if (!data || (Array.isArray(data) && data.length === 0))
