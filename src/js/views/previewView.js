d2b06338df8f2e03cadbc1b1df6a46827358a3c8
mstanka <mice78.mm@gmail.com>
2021-02-16 19:59:16 +0100

    add user icon

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -1,11 +1,11 @@
 import View from './View.js';
-
+import icons from 'url:../../img/icons.svg';
 class PreviewView extends View {
   _parentElement = '';
 
   _generateMarkup() {
     const id = window.location.hash.slice(1);
-   
+
     return `
      <li class="preview">
         <a class="preview__link ${
@@ -17,7 +17,12 @@ class PreviewView extends View {
           <div class="preview__data">
             <h4 class="preview__title">${this._data.title}</h4>
             <p class="preview__publisher">${this._data.publisher}</p>           
-          </div>
+            <div class="preview__user-generated ${this._data.key ? '' : 'hidden'}">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>
+          </div>          
         </a>
       </li>
     `;
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -78,14 +78,19 @@ class RecipeView extends View {
             </div>
           </div>
 
-          <div class="recipe__user-generated">
-            
-          </div>
+          <div class="recipe__user-generated ${
+              this._data.key ? '' : 'hidden'
+            }">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>            
+         
           <button class="btn--round btn--bookmark">
             <svg class="">
               <use href="${icons}#icon-bookmark${
-                this._data.bookmarked ? '-fill' : ''
-                }"></use>
+              this._data.bookmarked ? '-fill' : ''
+            }"></use>
             </svg>
           </button>
         </div>
